5c6cc7b9d3576edf17da938f411d4f7f5cea975d
marp98 <manuelroshardt@gmail.com>
2023-01-12 23:25:05 -0500

    three.js fundamentals completed - created scene with three rotating cubes

--- fundamentals-vite/main.js
+++ fundamentals-vite/main.js
@@ -18,12 +18,45 @@ function main() {
   const boxDepth = 1;
   const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 }); // greenish blue
+  function makeInstance(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({ color });
 
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-  renderer.render(scene, camera);
+    cube.position.x = x;
+
+    return cube;
+  }
+
+  const cubes = [
+    makeInstance(geometry, 0x44aa88, 0),
+    makeInstance(geometry, 0x8844aa, -2),
+    makeInstance(geometry, 0xaa8844, 2),
+  ];
+  scene.add(cubes);
+
+  const color = 0xffffff;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
+
+  function render(time) {
+    time *= 0.001; // convert time to seconds
+
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * 0.1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
+    });
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 }
 
 main();
